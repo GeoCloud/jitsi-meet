cad58a5a93b4d4ed20637d6637b40711788a928a
Rinat <iam.rinat.gabdrashitov@gmail.com>
2022-08-01 13:41:59 +0300

    add some files

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -359,27 +359,29 @@ class WelcomePage extends AbstractWelcomePage {
                 <div className = 'welcome-footer-padded'>
                     <div className = 'welcome-footer-row-block welcome-footer--row-1'>
                         <div className = 'welcome-footer-row-1-text'>{t('welcomepage.jitsiOnMobile')}</div>
-                        <a
-                            className = 'welcome-badge'
-                            href = { MOBILE_DOWNLOAD_LINK_IOS }>
-                            <img
-                                alt = { t('welcomepage.mobileDownLoadLinkIos') }
-                                src = './images/app-store-badge.png' />
-                        </a>
-                        <a
-                            className = 'welcome-badge'
-                            href = { MOBILE_DOWNLOAD_LINK_ANDROID }>
-                            <img
-                                alt = { t('welcomepage.mobileDownLoadLinkAndroid') }
-                                src = './images/google-play-badge.png' />
-                        </a>
-                        <a
-                            className = 'welcome-badge'
-                            href = { MOBILE_DOWNLOAD_LINK_F_DROID }>
-                            <img
-                                alt = { t('welcomepage.mobileDownLoadLinkFDroid') }
-                                src = './images/f-droid-badge.png' />
-                        </a>
+                        <div className="welcome-footer-links">
+                            <a
+                                className = 'welcome-badge'
+                                href = { MOBILE_DOWNLOAD_LINK_IOS }>
+                                <img
+                                    alt = { t('welcomepage.mobileDownLoadLinkIos') }
+                                    src = './images/app-store-badge.png' />
+                            </a>
+                            <a
+                                className = 'welcome-badge'
+                                href = { MOBILE_DOWNLOAD_LINK_ANDROID }>
+                                <img
+                                    alt = { t('welcomepage.mobileDownLoadLinkAndroid') }
+                                    src = './images/google-play-badge.png' />
+                            </a>
+                            <a
+                                className = 'welcome-badge'
+                                href = { MOBILE_DOWNLOAD_LINK_F_DROID }>
+                                <img
+                                    alt = { t('welcomepage.mobileDownLoadLinkFDroid') }
+                                    src = './images/f-droid-badge.png' />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
